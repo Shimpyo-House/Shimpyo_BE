1d5757838e2e7575096ea1cb01be28b2fb6b9e8d
jo0oy <rkfkrh07@naver.com>
2023-11-30 17:24:56 +0900

    #64 test: 별점 MockData Insert Service 통합 테스트 작성

---
+++ src/test/resources/testdata/star-mockdata-setup.sql
@@ -0,0 +1,39 @@
+insert into member (id, name, email, password, photo_url, authority)
+values (1, 'member1', 'member1@email.com', 'password1', 'photoUrl', 'ROLE_USER');
+
+insert into member (id, name, email, password, photo_url, authority)
+values (2, 'member2', 'member2@email.com', 'password2', 'photoUrl', 'ROLE_USER');
+
+insert into member (id, name, email, password, photo_url, authority)
+values (3, 'member3', 'member3@email.com', 'password3', 'photoUrl', 'ROLE_USER');
+
+insert into member (id, name, email, password, photo_url, authority)
+values (4, 'member4', 'member4@email.com', 'password4', 'photoUrl', 'ROLE_USER');
+
+insert into member (id, name, email, password, photo_url, authority)
+values (5, 'member5', 'member5@email.com', 'password5', 'photoUrl', 'ROLE_USER');
+
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (1, '호텔1', 'HOTEL', '호텔1 설명', 0.0, '호텔1 사진 url', '서울시 강남구 한남동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (2, '호텔2', 'HOTEL', '호텔2 설명', 0.0, '호텔2 사진 url', '서울시 송파구 잠실동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (3, '호텔3', 'HOTEL', '호텔3 설명', 0.0, '호텔3 사진 url', '서울시 강남구 한남동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (4, '호텔4', 'HOTEL', '호텔4 설명', 0.0, '호텔4 사진 url', '서울시 송파구 잠실동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (5, '호텔5', 'HOTEL', '호텔5 설명', 0.0, '호텔5 사진 url', '서울시 강남구 한남동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (6, '호텔6', 'HOTEL', '호텔6 설명', 0.0, '호텔6 사진 url', '서울시 송파구 잠실동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (7, '호텔7', 'HOTEL', '호텔7 설명', 0.0, '호텔7 사진 url', '서울시 강남구 한남동');
+
+insert into product (id, name, category, description, star_avg, thumbnail, address)
+values (8, '호텔8', 'HOTEL', '호텔8 설명', 0.0, '호텔8 사진 url', '서울시 송파구 잠실동');
